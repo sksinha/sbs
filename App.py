5425285e477f9dbe8c1c197f48e4ae10dd26573d
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-13 13:34:36 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_metrics import metric, metric_row
+
+st.write("## Here's a single figure")
+metric("Metric 0", 0)
+
+st.write("## ... and here's a row of them")
+metric_row(
+    {
+        "Metric 1": 100,
+        "Metric 2": 200,
+        "Metric 3": 300,
+        "Metric 4": 400,
+        "Metric 5": 500,
+    }
+)
